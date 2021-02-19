089cfd98aca1cda1da1625e83576576556a963c1
jarod25 <63459117+jarod25@users.noreply.github.com>
2021-02-19 19:37:11 +0100

    decrypt fini

--- cc_attaque_cesar.py
+++ cc_attaque_cesar.py
@@ -3,25 +3,24 @@ def decrypt(text,step):
 
     for i in range (len(text)):
         char=text[i]
-        if not char.islower():
+        if not char.isalpha():
             result += char
         elif char.isupper():
-            result += chr((ord(char)+s-65)%26+65)
+            result += chr((ord(char)+step-65)%26+65)
         else:
-            result += chr((ord(char)+s-97)%26+97)
-        return result
+            result += chr((ord(char)+step-97)%26+97)
+    return result
 try:
-    f=open(sys.argv[1], 'r')
-    f1=open("message-dechiffre.txt", 'w')
+    f=open("C:/Users/PC/Downloads/Cesar-Code-Participants/message-chiffre.txt", 'r') 
+    f1=open("C:/Users/PC/Documents/GitHub/cc_attaque_cesar/cc_attaque_cesar/message-dechiffre.txt", 'w')
 
     step=int(input("Entrez le nombre de décalage des lettres : "))
-    text=f. ()
-    f1. (encrypt(text, 26-step))
+    text=f.read()
+    f1.write(decrypt(text, 26-step))
     f.close()
     f1.close()
     print("Le message chiffré est disponible sous le nom message-dechiffre.txt")
 
-except Exception:
-    print("Le fichier n'est pas existant.")
-    
-    
+except Exception as e:
+    print("Le fichier n'est pas existant.", e)
+
